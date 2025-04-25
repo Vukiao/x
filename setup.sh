34a3f3e5e1fb9460520a43216a502b7e6cf14f57
Vukiao <khaiaabbccas05@gmail.com>
2025-04-25 19:35:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,8 @@ apt update -y
 
 # Cài đặt các thư viện npm cần thiết
 npm install axios hpack chalk fs https-proxy-agent http-proxy-agent async request puppeteer-extra puppeteer-extra-plugin-stealth puppeteer-extra-plugin-adblocker ua-parser-js user-agents crypto os colors random-referer puppeteer
-
+npm install request random-referer user-agents puppeteer puppeteer-extra puppeteer-extra-plugin-stealth async colors hpack puppeteer puppeteer-extra puppeteer-extra-plugin-stealth async
+sudo apt install ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils -y
 # Cài đặt Python và các gói pip cần thiết
 apt install python3.12-venv -y
 python3 -m venv venv
